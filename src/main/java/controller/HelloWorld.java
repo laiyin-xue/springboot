0497a15bcf80ceff03f338db10db913b9a9a7f03
Laiyin XUE <laiyin.xue@fullsix.com>
2015-06-16 14:37:52 +0200

    some simple examples

---
+++ src/main/java/controller/HelloWorld.java
@@ -0,0 +1,39 @@
+package controller;
+
+import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloWorld {
+
+    @RequestMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/printMessage")
+    public String printMessage(@RequestParam String message) {
+        return message;
+    }
+
+    @RequestMapping("/printHttpRequests")
+    public String printHttpRequestParams(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getParameterMap()
+                .values()
+                .stream()
+                .map(value -> StringUtils.join(value, ""))
+                .collect(Collectors.joining(", "));
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class, args);
+    }
+}
